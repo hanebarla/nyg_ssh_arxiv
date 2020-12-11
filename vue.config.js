fee1564f958d06405319f3330ab99442ac9b22d2
hanebarla <barla.wings@gmail.com>
2020-12-11 23:08:00 +0900

    create docs

--- vue.config.js
+++ vue.config.js
@@ -1,7 +1,5 @@
 module.exports = {
-    devServer: {
-        proxy: {
-            '/arxiv': 'https://script.google.com/macros/s/AKfycbwkY30DoRmckEogVP8QB-XxhTvtJleGM8w6HTHCMFxB4iwrmPMN/exec'
-        }
-    }
+    outputDir: 'docs',
+    assetDir: './',
+    publicPath: './'
 }
